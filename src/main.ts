1da98c8a30a216d7d0927c50bc5b83171665f5d8
Harrsh Patel <me@harrsh.com>
2024-06-13 19:23:55 +0530

    Tweaks

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { ErrorInterceptor } from './common/interceptors/error.interceptor';
 import { NotFoundInterceptor } from './common/interceptors/not-found.interceptor';
+import helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.use(helmet());
+    app.enableCors();
 
     // Set global prefix to all routes as /api/v1
     app.setGlobalPrefix('api/v1');
